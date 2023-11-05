a594877052dc3c999295f47afd4b0c9e673b3b5f
Artem62512 <144590995+Artem62512@users.noreply.github.com>
2023-11-05 12:51:56 +0300

    Update fstream.cpp

--- fstream.cpp
+++ fstream.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 int main()
 {
-    ofstream fout ("input.txt");
-    fout << "Hallo, word!";
-    fout.close();
-    ifstream a ("input.txt");
-    string x;
+    ofstream fout ("input.txt"); // создание текстового документа
+    fout << "Hallo, word!"; // запись в текстового сообщения
+    fout.close(); // закрытие текстового документа 
+    ifstream a ("input.txt"); // открытие документа для считывания информации
+    string x; // объявление строки
 
-    getline (a, x);
-    cout << x << endl;
-    a.close();
+    getline (a, x); // запись в строку информации из текстового документа 
+    cout << x << endl; // вывод строки в консоль
+    a.close(); // закрытие документа 
 
 }
